2b94afd581b637f7912626ce420ba3dff99d345e
Neto <aguinaldoanselmo5@gmail.com>
2025-05-15 22:22:17 -0300

    ADD gerar.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 let index = 0;
 const carrossel = document.getElementById("carrossel");
 const itemLargura = 320;
+const divImagemGerada = document.getElementById("resultado-imagem");
 
 function avancar(){
     if(index < carrossel.children.length - 1){
@@ -15,3 +16,8 @@ function voltar() {
       carrossel.style.transform = `translateX(-${itemLargura * index}px)`;
     }
   }
+
+
+function ativarImagem(){
+  divImagemGerada.style.display = 'block';
+}
